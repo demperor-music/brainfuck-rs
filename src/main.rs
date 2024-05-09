98702d7a171c4e2b9b385797c8fea8dc18e586c6
Demperor <demperor@Hades-BSD>
2024-05-09 10:27:21 +0200

    i may or may not have written comments and prompts in Italian. Whoopsies

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ fn main() {
 
     print!(
         "
-Scegli cosa fare:
-1. Nuovo codice
-2. Apri file
+What do you want to do?:
+1. Write code
+2. Open File
 > "
     );
     stdout().flush().unwrap();
@@ -18,23 +18,23 @@ Scegli cosa fare:
 
     let code: Vec<u8> = match choice as char {
         '1' => {
-            println!("\n===INSERISCI CODICE===\n");
+            println!("\n===INSERT CODE===\n");
             input_long()
         },
         '2' => {
-            let filename = input_string("Inserisci nome completo del file: ");
+            let filename = input_string("Enter file's full pathname: ");
             fs::read_to_string(filename.trim()).unwrap().as_bytes().to_vec()
         }
         _ => return,
     };
     
-    println!("\n===CODICE===\n\n{}", String::from_utf8(code.clone()).expect("UTF-8 INVALIDO"));
+    println!("\n===SOURCE CODE===\n\n{}", String::from_utf8(code.clone()).expect("INVALID UTF-8"));
 
-    println!("\n===ESECUZIONE CODICE===");
+    println!("\n===CODE EXECUTION===");
 
     let mut mem: [u8; 256] = [0; 256];
     let mut pos: usize = 0;
-    let mut pc: usize = 0; // er program counter
+    let mut pc: usize = 0; // program counter
 
     while pc < code.len() {
         match code[pc] as char {
